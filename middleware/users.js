55fe2f8bf6543a4540ac18f137d4710f555ce6f5
Jason <147480420+JsonSept@users.noreply.github.com>
2024-03-11 16:57:49 +0200

    Update users.js

--- middleware/users.js
+++ middleware/users.js
@@ -2,6 +2,16 @@ import bcrypt from 'bcrypt'
 import cookieParser from 'cookie-parser'
 import jwt from 'jsonwebtoken'
 
+const authenticate = (req,res,next) =>{
+    let {cookie} = req.headers
+    let tokenInHeader = cookie && cookie.split('=')[1]
+    if(tokenInHeader===null) res.sendStatus(401)
+    jwt.verify(tokenInHeader,process.env.SECRET_KEY,(err,user)=>{
+if(err) return res.sendStatus(403)
+req.user = user
+next()
+})
+}
 
 const auth = async (req,res,next) => { 
     const {email, userPass} = req.body
